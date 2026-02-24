3d5eb6cdc7efbedcaa9a07d0efb415541d16793e
JhansiNagaBhavani <kodurijhansi441@gmail.com>
2026-02-24 12:15:14 +0530

    My-Portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/My portfolio/',
+  base: '/My-portfolio/',
   plugins: [react()],
   server: {
     proxy: {
